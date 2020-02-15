79175791f0efee0a668874db305402b82d3b3b8d
ehabhussein <ehab.hussein@msg.group>
2020-02-15 02:10:09 +0100

    Fetching basic rover information

--- src/public/client.js
+++ src/public/client.js
@@ -1,7 +1,12 @@
 let store = {
-    user: { name: "Student" },
+    user: {
+        name: "Student"
+    },
     apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    rovers: ['curiosity', 'opportunity', 'spirit'],
+    recent_photos: [],
+    current_rover: 'spirit',
+    rover: ''
 }
 
 // add our markup to the page
@@ -13,18 +18,60 @@ const updateStore = (store, newState) => {
 }
 
 const render = async (root, state) => {
+    console.log("xxxxxxxxxxxxxxxxxxxx")
     root.innerHTML = App(state)
 }
 
 
+// // create content
+// const App = (state) => {
+//     let {
+//         max_date,
+//         recent_photos,
+//         current_rover,
+//         apod
+//     } = state
+
+//     return `
+//         <header></header>
+//         <main>
+//             ${RoverSelector()}
+//             ${RoverInformation(current_rover, max_date)}
+
+//             ${Greeting(store.user.name)}
+//             <section>
+//                 <h3>Put things on the page!</h3>
+//                 <p>Here is an example section.</p>
+//                 <p>
+//                     One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+//                     the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
+//                     This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
+//                     applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
+//                     explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
+//                     but generally help with discoverability of relevant imagery.
+//                 </p>
+//                 ${ImageOfTheDay(apod)}
+//             </section>
+//         </main>
+//         <footer></footer>
+//     `
+// }
+
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
+    let {
+        rover,
+        recent_photos,
+        current_rover,
+        apod
+    } = state
 
     return `
         <header></header>
         <main>
+        
             ${Greeting(store.user.name)}
+            ${RoverInformation(current_rover, rover)}
             <section>
                 <h3>Put things on the page!</h3>
                 <p>Here is an example section.</p>
@@ -37,6 +84,7 @@ const App = (state) => {
                     but generally help with discoverability of relevant imagery.
                 </p>
                 ${ImageOfTheDay(apod)}
+                ${Carousel(recent_photos)}
             </section>
         </main>
         <footer></footer>
@@ -52,6 +100,7 @@ window.addEventListener('load', () => {
 
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
 const Greeting = (name) => {
+    console.log("Greeting component called")
     if (name) {
         return `
             <h1>Welcome, ${name}!</h1>
@@ -65,15 +114,17 @@ const Greeting = (name) => {
 
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (apod) => {
-
+    console.log("ImageOfTheDay Component")
     // If image does not already exist, or it is not from today -- request it again
     const today = new Date()
     const photodate = new Date(apod.date)
     console.log(photodate.getDate(), today.getDate());
 
     console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
+    if (!apod || apod.date === today.getDate()) {
         getImageOfTheDay(store)
+        //getRoverMaxDate(store)
+        //getRoverRecentImages(store)
     }
 
     // check if the photo of the day is actually type video!
@@ -91,18 +142,124 @@ const ImageOfTheDay = (apod) => {
     }
 }
 
+const RoverSelector = () => {
+    rovers_text = ""
+    store.rovers.map(rover => rovers_text += `<p>${rover}</p>`)
+    return (`
+        <select id="roverSelector">
+            ${store.rovers.map(rover => `<option value="${rover}">${rover}</option>`)}
+        </select>
+    `)
+}
+
+RoverInformation = (rover_name, rover) => {
+    console.log("Start rover info comp", rover)
+
+    // TODO: make sure to check if you really need to fetch the info
+    if( rover === '' ) {
+        console.log("We will fetch, Rover should be empty", rover)
+        getRoverInfo(rover_name, store)
+    } else  {
+        return (`
+            <h2> Rover Information </h2>
+            <ul> 
+                <li> <lable> Rover Max Date: <label> ${rover.max_date} </li>
+                <li> <lable> Rover Landing Date: <label> ${rover.landing_date} </li>
+                <li> <lable> Rover Launch Date: <label> ${rover.launch_date} </li>
+                <li> <lable> Rover Status: <label> ${rover.status} </li>
+            </ul>
+            
+             
+        `)
+    }
+}
+
+const Carousel = (recent_photos) => {
+    if (recent_photos.length === 0) {
+        getRoverRecentImages(store)
+    }
+
+    console.log("Corasel component ", recent_photos)
+    return (`
+        ${recent_photos.map(photo => `<img src="${photo.img_src}">`)}
+    `)
+}
+
 // ------------------------------------------------------  API CALLS
 
 // Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+const getImageOfTheDay = async (state) => {
+    let {
+        apod
+    } = state
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => { 
-            console.log(apod)
-            updateStore(store, { apod })
-        })
+    const response = await fetch(`http://localhost:3000/apod`)
+    apod = await response.json()
 
-    return data
+    console.log("getImageOfTheDay: store will be updated")
+    updateStore(store, {
+        apod
+    })
+    
+    //return data
+}
+
+const getRoverMaxDate = async (state) => {
+    // make the request with varies rover names
+    //Rovers names are
+    // Curiosity, Opportunity, and Spirit 
+
+    let {
+        max_date
+    } = state
+    const response = await fetch(`http://localhost:3000/roverInfo/${state.current_rover}`)
+    const result = await response.json()
+    console.log(result)
+    console.log(result.info.photo_manifest.max_date)
+    max_date = result.info.photo_manifest.max_date
+    //updateStore(store, { max_date })
+}
+
+const getRoverInfo = async (current_rover, state) => {
+    // make the request with varies rover names
+    //Rovers names are
+    // Curiosity, Opportunity, and Spirit 
+
+    let {
+        rover
+    } = state
+
+    const dateResponse = await fetch(`http://localhost:3000/roverInfo/${current_rover}`)
+    const dateResult = await dateResponse.json()
+
+    // max_date = dateResult.info.photo_manifest.max_date
+    // landing_date = dateResult.info.photo_manifest.landing_date
+    // launch_date = dateResult.info.photo_manifest.launch_date
+    // total_photos = dateResult.info.photo_manifest.total_photos
+    rover = dateResult.info.photo_manifest
+    console.log(rover)
+
+    console.log("getRoverInfo: store will be updated")
+    updateStore(store, { rover })
+}
+
+const getRoverRecentImages = async (state) => {
+    // make the request with varies rover names
+    //Rovers names are
+    // Curiosity, Opportunity, and Spirit 
+
+    let {
+        max_date,
+        recent_photos
+    } = state
+
+    const dateResponse = await fetch(`http://localhost:3000/roverInfo/${state.current_rover}`)
+    const dateResult = await dateResponse.json()
+
+    max_date = dateResult.info.photo_manifest.max_date
+    console.log("dsjds jkdjsk jdsjkd jsj",max_date)
+    const response = await fetch(`http://localhost:3000/roverRecent?rover=${state.current_rover}&date=${max_date}`)
+    const result = await response.json()
+    recent_photos = result.data.photos
+    updateStore(store, { max_date, recent_photos })
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,9 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import { Observable } from 'rxjs'
+import {
+  Observable
+} from 'rxjs'
 import express from 'express';
 import bodyparser from 'body-parser'
 import path from 'path'
@@ -24,10 +26,10 @@ console.log(path.join(__dirname, '../src/public'))
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-app.use("/images", function (req, res) {
+app.use("/test", function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({
-    images: ["asdf", "asdfasd f", "ehab work on thesis "]
+    images: ["test", "test1", "test3"]
   }));
 });
 
@@ -50,15 +52,66 @@ function getObservableFromFetch(url) {
   })
 }
 
-const requestObserver$ = getObservableFromFetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+const testObserver$ = getObservableFromFetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
 
 app.get('/apod', async (req, res) => {
-  requestObserver$.subscribe(image => {
+  console.log("pod")
+  testObserver$.subscribe(image => {
     res.send({
       image
     })
   })
 })
+
+const roverObserver$ = getObservableFromFetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.API_KEY}`)
+
+app.get('/rover', async (req, res) => {
+  roverObserver$.subscribe(image => {
+    res.send({
+      image
+    })
+  })
+})
+
+app.get('/roverInfo/:rover_name', async (req, res) => {
+  console.log(req.params.rover_name)
+  const roverInfoObserver$ = 
+      getObservableFromFetch(
+        `https://api.nasa.gov/mars-photos/api/v1/manifests/${req.params.rover_name}?api_key=${process.env.API_KEY}`
+        );
+
+    roverInfoObserver$.subscribe(info => {
+    res.send({
+      info
+    })
+  })
+})
+
+app.get('/roverRecent', async (req, res) => {
+  const max_date = req.query['date']
+  const rover_name = req.query['rover']
+
+  const roverInfoObserver$ = 
+      getObservableFromFetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover_name}/photos?earth_date=${max_date}&api_key=${process.env.API_KEY}`
+        );
+
+    roverInfoObserver$.subscribe(data => {
+    res.send({
+      data
+    })
+  })
+})
+
+
+//Rovers names are
+// Curiosity, Opportunity, and Spirit 
+
+// To get the rover max date, landing and launch date
+// https://api.nasa.gov/mars-photos/api/v1//manifests/Curiosity?api_key=DEMO_KEY
+
+// To get the recent photos taken on the max_date
+//https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2019-09-28&api_key=DEMO_KEY
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
